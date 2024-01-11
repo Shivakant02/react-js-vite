693c12e8b6bc9d7a4af798ba4df1be68759e7fe3
Shivakant <kants6397@gmail.com>
2024-01-11 14:09:18 +0530

    mega Project database setup

---
+++ Frontend mega project/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+     <h1>Hi there,new Project begins</h1>
+    </>
+  )
+}
+
+export default App
